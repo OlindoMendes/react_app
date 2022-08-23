d09146fb72fd1bad6e510991f9be900ff8a2815f
OlindoMendes <olindodecarvalho@hotmail.com>
2022-08-23 17:02:20 -0300

    feat: implement the filter method

--- src/Components/ProductCategory.jsx
+++ src/Components/ProductCategory.jsx
@@ -2,9 +2,22 @@ import React, { useState } from "react";
 import Products from "../productsCategory.json";
 
 const ProductCategory = () => {
-  const [products] = useState(Products);
+  const [products, getProducts ] = useState(Products);
   const productData = products.data.nodes;
-  console.log(products)
+
+  function filterResult(category){
+   
+    
+    return   productData.filter((product) => {
+      let data = ''
+      if(product.category.name === category){
+        data =  product.category.name
+        return data
+      } 
+    });
+  
+  };
+
 
   return (
     <>
@@ -16,32 +29,46 @@ const ProductCategory = () => {
         </div>
         <div className="row">
           <div className="col">
-            <button className="btn">Todos</button>
-            <button className="btn"> 1</button>
-            <button className="btn">2</button>
-            <button className="btn">3</button>
+            <button className="btn" onClick={() => getProducts(Products)}>
+              Todos
+            </button>
+            <button className="btn" onClick={(category) => filterResult("Aerosol")}>
+              Aerosol
+            </button>
+            <button className="btn" onClick={() => filterResult("Jabón Barra")}>
+              Sabão Barra
+            </button>
+            <button
+              className="btn"
+              onClick={() => filterResult("Jabón Líquido")}
+            >
+              Sabão Líquido
+            </button>
+            <button className="btn" onClick={() => filterResult("Talco")}>
+              Talco
+            </button>
           </div>
           <div className="col">
             <div className="cards">
-              
+              {filterResult('Talco').map((product, key) => {
+                return (
+                  <div className="card" key={key}>
+                    <div className="card-header">
+                      <img
+                        src={product.images[0].asset}
+                        alt={product.category.name}
+                      />
+                    </div>
+                    <div className="card-body"></div>
+                    <h2 className="titleProduct">{product.name}</h2>
+                    <p className="category">{product.shortDescription}</p>
+                    <div className="card-footer"></div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-        {productData.map((product, key) => {
-          return (
-            <div className="cards" key={key}>
-              <div className="card" key="">
-                <div className="card-header">
-                  <img src={product.images[0].asset} alt={product.name}  />
-                </div>
-                <div className="card-body"></div>
-                <h2 className="titleProduct">{product.name}</h2>
-                <p>{product.shortDescription}</p>
-                <div className="card-footer"></div>
-              </div>
-            </div>
-          );
-        })}
       </div>
     </>
   );
